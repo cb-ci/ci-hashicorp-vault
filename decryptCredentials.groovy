a0ed92a1e270d43b176ee0f9fc6c26199ffa2af0
cccaternberg <acaternberg@cloudbees.com>
2024-10-24 17:58:21 +0200

    update

--- decryptCredentials.groovy
+++ decryptCredentials.groovy
@@ -10,11 +10,10 @@ creds.each { println("Credentials class: " + it.id + it.class) }
 println()
 
 for(c in creds) {
-    if (c instanceof com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl){
-        println(String.format("id=%s  desc=%s user=%s pass=%s", c.id, c.description, c.username, c.password))
+    if(c instanceof com.cloudbees.jenkins.plugins.vault.credentials.VaultSshPrivateKeyCredentials){
+        println(String.format("id=%s desc=%s user=%s key=%s", c.id, c.description, c.username, c.privateKey))
     }
     if (c instanceof com.cloudbees.jenkins.plugins.vault.credentials.VaultUsernamePasswordCredentials){
         println(String.format("id=%s  desc=%s user=%s pass=%s", c.id, c.description,c.username, c.password))
     }
-
 }
