fcef008ad2150bd3719ae166b048ea6146b96c27
cccaternberg <acaternberg@cloudbees.com>
2024-10-25 11:20:43 +0200

    update

--- 04_vaultCreateSecrets.sh
+++ 04_vaultCreateSecrets.sh
@@ -5,24 +5,27 @@ source ./setEnv.sh
 #kubectl config set-context --current --namespace=$NAMESPACE_VAULT
 #kubectl exec -n $NAMESPACE_VAULT  vault-0 -- sh -c "vault secrets enable -mount=secrets kv"
 
-# Create Secrets KV1
-# Enable kv1 on path secrets
+
+# Enable kv1 on path $SECRET_PATH_KV1
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault secrets enable -path=$SECRET_PATH_KV1 kv"
+# Create Secrets KV1
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  put  $SECRET_PATH_KV1/cloudbees-login username=admin password=TEST123"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  list -format=json $SECRET_PATH_KV1"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  get  -format=json $SECRET_PATH_KV1/cloudbees-login"
 
-# Create Secrets KV2
-# enable kv2 on path secrets-kv2
+
+# enable kv2 on path $SECRET_PATH_KV2
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault secrets enable -path=$SECRET_PATH_KV2 kv-v2"
+# Create Secrets KV2
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  put  $SECRET_PATH_KV2/cloudbees-login username=admin password=TEST123"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv metadata put -custom-metadata=Membership=CloudBees $SECRET_PATH_KV2/cloudbees-login"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  list -format=json $SECRET_PATH_KV2"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  get  -format=json $SECRET_PATH_KV2/cloudbees-login"
 
-# Create Secrets for test deployment yamnl/vaultTestDeployment.yaml
-# enable kv2 on path secrets
+
+# enable kv2 on path $SECRET_PATH_TEST
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault secrets enable -path=$SECRET_PATH_TEST kv"
+# Create Secrets for test deployment yamnl/vaultTestDeployment.yaml
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  put  $SECRET_PATH_TEST/cloudbees-login username=admin password=TEST123"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  list -format=json $SECRET_PATH_TEST"
 kubectl exec -n $NAMESPACE_VAULT vault-0 -- sh -c "vault kv  get  -format=json $SECRET_PATH_TEST/cloudbees-login"
